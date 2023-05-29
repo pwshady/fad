bbddb30b44e10f5db5cb00f8a0f827e9a9db9bdb
Pwshady <pwshady@gmail.com>
2023-05-29 23:39:04 +0300

    290523

--- app/modules/curl/Controller.php
+++ app/modules/curl/Controller.php
@@ -10,6 +10,24 @@ class Controller
 
     use t\TSingleton;
 
+    public function curl_init()
+    {
+        return curl_init();
+    }
 
+    public function curl_setopt($ch, $option, $value)
+    {
+        return curl_setopt($ch, $option, $value);
+    }
+
+    public function curl_exec($ch)
+    {
+        return curl_exec($ch);
+    }
+
+    public function curl_close($ch)
+    {
+        return curl_close($ch);
+    }
 
 }
--- app/widgets/parser/Controller.php
+++ app/widgets/parser/Controller.php
@@ -9,6 +9,9 @@ use fa\App;
 class Controller extends WidgetController
 {
 
+    private string $result = 'unknown result';
+    private array $configs;
+
     public function run()
     {
         parent::run();
@@ -16,7 +19,98 @@ class Controller extends WidgetController
     }
 
     private function job(){
+        $url_name = 'url';
+        $key_name = 'key';
+        $key = '';
+        $url = '';
+        $this->configs = $this->model->getConfigs();
+        if (array_key_exists('url_name', $this->configs) && $this->configs['url_name'] != '') {
+            $url_name = $this->configs['url_name'];
+        }
+        if (array_key_exists('key_name', $this->configs) && $this->configs['key_name'] != '') {
+            $key_name = $this->configs['key_name'];
+        }
+        if (array_key_exists($url_name, $this->params) && $this->params[$url_name] != '') {
+            $url = $this->params[$url_name]['value'];
+            unset($this->params[$url_name]);
+        } else {
+            $this->result = 'Error. Url not set';
+            return;
+        }
+        if (array_key_exists($key_name, $this->params) && $this->params[$key_name] != '') {
+            $key = $this->params[$key_name]['value'];
+            unset($this->params[$key_name]);
+        }
+        if (array_key_exists('key', $this->configs) && $key != $this->configs['key']) {
+            $this->result = 'Error. Wrong key';
+            return;
+        }
+        if (array_key_exists('type_pagination', $this->configs) && $this->configs['type_pagination'] != '') {
+            switch ($this->configs['type_pagination']) {
+                case 'next':
+                    echo 'next';
+            }
+        } else {
+            $html = self::getPage($url);
+            $data = self::parsPage($html);
+        }
+    }
 
+    private function getPage($url)
+    {
+        $curl = App::$app->getModul('curl')['object'];
+        $ch = $curl->curl_init();
+        $curl->curl_setopt($ch, CURLOPT_URL, "example.com");
+        $curl->curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $html = $curl->curl_exec($ch);
+        $curl->curl_close($ch);
+        return $html;
+    }
+
+    private function parsPage($html)
+    {
+        $data = [];
+        if (array_key_exists('mask', $this->configs) && is_array($this->configs['mask'])) {
+            $mask = $this->configs['mask'];
+            $data = self::parsDatas($html, $mask);
+        } else {
+            $this->result = 'Error. Mask not set';
+        }
+        return $data;
+    }
+
+    private function parsDatas($html, $mask)
+    {
+        $data = [];
+            foreach ($mask as $key=>$value) {
+                $revers = false;
+                $type = 'single';
+                $recurs = false;
+                $recurs_mask = [];
+                debug($mask);
+                if (array_key_exists('data', $value) && is_array($value['data'])) {
+                    if (array_key_exists('name', $value['data']) && is_string($value['data']['name']) && $value['data']['name'] != '') {
+                        $key = $value['data']['name'];
+                    }
+                    if (array_key_exists('type', $value['data']) && is_string($value['data']['type']) && ($value['data']['type'] == 'single' || $value['data']['type'] == 'multi')) {
+                        $type = $value['data']['type'];
+                    } else {
+                        $this->result = 'Error. Data type must be single or multi';
+                    }
+                    if (array_key_exists('revers', $value['data'])) {
+                        $revers = $value['data']['type'];
+                    }
+                    debug( $key);
+                
+                    debug( $type);
+                    debug( $revers);
+                    echo 'hhhhh';
+                } else {
+                    $this->result = 'Error. No data in the mask';
+                }
+                //echo htmlspecialchars($html);
+            }
+        return $data;
     }
 
     public function render()
@@ -25,8 +119,7 @@ class Controller extends WidgetController
         $widget['complete'] = 1;
         $labels = $this->model->setLabels();
         ob_start();
-        //debug($this->model->getConfigs());
-        debug($this->params);
+        echo $this->result;
         $html[0] = ob_get_clean();
         $widget['code'] = $html;
         App::$app->updateWidget($widget);
